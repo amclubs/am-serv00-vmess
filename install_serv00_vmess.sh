15649a8c0e18fc871b3e9ee15015480ad7e47c27
AM科技 <amclubss@gmail.com>
2024-11-08 10:42:55 +0800

    Update install_serv00_vmess.sh

--- install_serv00_vmess.sh
+++ install_serv00_vmess.sh
@@ -14,7 +14,12 @@ reading() { read -p "$(red "$1")" "$2"; }
 
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'d36c4d9f-31c4-45f1-8c64-102a6142001e'}
+
+#export UUID=${UUID:-'d36c4d9f-31c4-45f1-8c64-102a6142001e'}
+UUID=${1:-'d36c4d9f-31c4-45f1-8c64-102a6142001e'}
+export UUID
+echo "Using UUID: $UUID"
+
 export NEZHA_SERVER=${NEZHA_SERVER:-''} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''} 
